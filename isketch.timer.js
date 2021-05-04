5fde0dc305b2236c2c0a5ba55b2181e9ec1317ac
Dylan Bickerstaff <dylan@belowaverage.org>
2021-05-04 03:04:57 -0400

    Timer done!

--- isketch.timer.js
+++ isketch.timer.js
@@ -1,24 +1,40 @@
 var is_timer = document.getElementById("isketch-timer");
-var ctx = is_timer.getContext("2d");
+var ctx      = is_timer.getContext("2d");
 
-var count = 0;
-var radius = 50;
+var colorTM  = "pink";
+var stroke   = "pink";
+var radius   = 50;
+var originX  = 55;
+var originY  = 55;
+var startRad = (Math.PI / 2 * -1);
+var endRad   = (Math.PI / 2 * 3);
+var radDiff  = endRad - startRad;
+var timerMS  = 1000;
+var startMS  = 0;
+
+ctx.strokeStyle = stroke;
 
 function loop() {
     ctx.fillStyle = "#fff"
-    ctx.fillRect(0, 0, 100, 100);
-    
-    
+    ctx.fillRect(0, 0, is_timer.width, is_timer.height);
+    ctx.fillStyle = colorTM;
     ctx.beginPath();
-    ctx.arc(50, 50, 50, 1, 2);
-
+    ctx.moveTo(originX, originY);
+    var timerRatio = ((performance.now() - startMS) / timerMS);
+    ctx.arc(originX, originY, radius, startRad, (radDiff * timerRatio) + startRad);
+    ctx.lineTo(originX, originY);
+    ctx.fill();
     ctx.stroke();
-
+    if(timerRatio >= 1) {
+        startTimer();
+        return;
+    }
     requestAnimationFrame(loop);
 }
-requestAnimationFrame(loop);
 
+function startTimer() {
+    startMS = performance.now();
+    requestAnimationFrame(loop);
+}
 
-setInterval(function() {
-    count += Math.PI;
-}, 100);
+startTimer();

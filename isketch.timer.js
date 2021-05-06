bd628c6b396c7359bbee0fe47a74c191a8eae8f9
Dylan Bickerstaff <dylan.bickerstaff@cedarpoint.com>
2021-05-06 13:32:09 -0400

    Added sounds to timer.

--- isketch.timer.js
+++ isketch.timer.js
@@ -1,40 +1,59 @@
 var is_timer = document.getElementById("isketch-timer");
 var ctx      = is_timer.getContext("2d");
 
-var colorTM  = "pink";
-var stroke   = "pink";
+var colorTM  = "skyblue";
+var colorTM2 = "pink";
+var stroke   = "transparent";
+var warnAt   = 0.25;
 var radius   = 50;
 var originX  = 55;
 var originY  = 55;
 var startRad = (Math.PI / 2 * -1);
 var endRad   = (Math.PI / 2 * 3);
 var radDiff  = endRad - startRad;
-var timerMS  = 1000;
+var timerMS  = 10000;
 var startMS  = 0;
+var isTick   = false;
+var tickingr = 250;
+var ticking  = new Audio("isketch.timer.tick.ogg");
+var ding     = new Audio("isketch.timer.ding.ogg");
 
 ctx.strokeStyle = stroke;
+setInterval(tick, tickingr);
 
 function loop() {
+    var timerRatio = ((performance.now() - startMS) / timerMS);
     ctx.fillStyle = "#fff"
     ctx.fillRect(0, 0, is_timer.width, is_timer.height);
-    ctx.fillStyle = colorTM;
+    if(timerRatio >= 1) {
+        isTick = false;
+        ding.play();
+        return;
+    }
+    if(timerRatio >= warnAt) {
+        isTick = true;
+        ctx.fillStyle = colorTM2;
+    } else {
+        isTick = false;
+        ctx.fillStyle = colorTM;
+    }
     ctx.beginPath();
     ctx.moveTo(originX, originY);
-    var timerRatio = ((performance.now() - startMS) / timerMS);
-    ctx.arc(originX, originY, radius, startRad, (radDiff * timerRatio) + startRad);
+    ctx.arc(originX, originY, radius, (radDiff * timerRatio) + startRad, startRad);
     ctx.lineTo(originX, originY);
     ctx.fill();
     ctx.stroke();
-    if(timerRatio >= 1) {
-        startTimer();
-        return;
-    }
     requestAnimationFrame(loop);
 }
 
 function startTimer() {
     startMS = performance.now();
+    ctx.fillStyle = colorTM;
     requestAnimationFrame(loop);
 }
 
-startTimer();
+function tick() {
+    if(isTick) {
+        ticking.play();
+    }
+}
